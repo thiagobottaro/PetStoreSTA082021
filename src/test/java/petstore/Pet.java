51d1fe4040e1954d0fed0ba394dbf543ff911383
Thiago Padilha <documentos.thiago@gmail.com>
2021-08-12 22:06:04 -0300

    Finalizado o script do pet, feito o CRUD Create, Replace, Update e Delete.

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -29,7 +29,7 @@ public class Pet {
     }
 
     // Incluir - Create - Post
-    @Test  // Identifica o metodo ou funcao como um teste para o TestNG
+    @Test(priority = 1)  // Identifica o metodo ou funcao como um teste para o TestNG
     public void insertPet() throws IOException {
         String jsonBody=readJson("db/pet1.json");
 
@@ -48,11 +48,78 @@ public class Pet {
                 .statusCode(200) // status code da conexao com o servidor
                 .body("name", is("Kiara"))
                 .body("status", is("available"))
-                .body("category.name", is("dog"))
+                .body("category.name", is("AX2356554874"))
                 .body("tags.name", contains("sta")) // contains utilizar para verificar conteudo de array
+                //.extract()
         ;
 
         //id: 5864798277
+        //id: 5864798278
+        //id: 5864798279
+        //id: 5864798358
         //id: 9223372036854775807
     }
+
+    // (priority = 2) => sequencia para executar o teste. Executando pelo build.gradle
+    @Test(priority = 2)
+    public void getPet(){
+        String petId="5864798358";
+
+        String token=
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri+"/"+petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name",is("Kiara"))
+                .body("category.name", is("AX2356554874"))
+                .body("status", is("available"))
+                .body("tags.name", contains("sta"))
+        .extract()
+                .path("category.name")
+        ;
+        System.out.println("O token eh:" + token);
+    }
+
+     @Test(priority = 3)
+    public void updatePet() throws IOException {
+        String jsonBody=readJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Kiara"))
+                .body("status", is("sold"))
+        ;
+
+    }
+
+     @Test(priority = 4)
+     public void deletePet(){
+         String petId="5864798358";
+
+         given()
+                 .contentType("application/json")
+                 .log().all()
+         .when()
+                 .delete(uri+"/"+petId)
+         .then()
+                 .log().all()
+                 .statusCode(200)
+                 .body("code", is(200))
+                 .body("type", is("unknown"))
+                 .body("message", is(petId))
+         ;
+
+     }
+
 }
